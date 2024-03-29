4445438ea7277e2d81796c21dc8d2180e72b0991
Harmeet Singh <er.harmeetsingh@hotmail.com>
2024-03-29 17:48:04 -0600

    Fixing search debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,8 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
@@ -22,7 +21,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
-  };
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
